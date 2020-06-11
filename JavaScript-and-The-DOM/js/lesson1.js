9e20224dbac1d799be207fcc36c240aa94a66694
SondosSamii <sondos.sami.abuzeid@gmail.com>
2020-06-12 00:51:04 +0200

    Add Destructuring Arrays Quiz

--- JavaScript-and-The-DOM/js/lesson1.js
+++ JavaScript-and-The-DOM/js/lesson1.js
@@ -60,3 +60,32 @@ function createAnimalTradingCardHTML(animal) {
 }
 
 console.log(createAnimalTradingCardHTML(cheetah));
+
+
+const point = [10, 25, -34];
+
+const [x, , z] = point;
+
+console.log(x, z);
+
+/*
+ * Programming Quiz: Destructuring Arrays (1-3)
+ *
+ * Use destructuring to initialize the variables `one`, `two`, and `three`
+ * with the colors from the `things` array.
+ */
+
+const things = ['red', 'basketball', 'paperclip', 'green', 'computer', 'earth', 'udacity', 'blue', 'dogs'];
+
+//const one = things;
+//const two = '';
+//const three = '';
+
+const [one, , , two, , , , three] = things;
+
+const colors = `List of Colors
+1. ${one}
+2. ${two}
+3. ${three}`;
+
+console.log(colors);
