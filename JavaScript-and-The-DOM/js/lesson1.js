5b290be60e7999e22939cba4bca2753257c3524a
SondosSamii <sondos.sami.abuzeid@gmail.com>
2020-06-11 21:06:04 +0200

    Add Using Let and Const Quiz

---
+++ JavaScript-and-The-DOM/js/lesson1.js
@@ -0,0 +1,16 @@
+function getClothing(isCold) {
+    if (isCold) {
+        const freezing = 'Grab a jacket!';
+    }
+    else {
+        const hot = 'It is a short kind of a day.';
+        console.log(freezing);
+    }
+}
+
+// getClothing(false);
+
+let instructor = 'Sondos';
+instructor = 'Amr';
+console.log(instructor);
+
