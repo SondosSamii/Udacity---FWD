7ac068fae885dd7413eac3254fc2a6fba4cfa8d2
SondosSamii <sondos.sami.abuzeid@gmail.com>
2020-06-13 21:08:51 +0200

    Concatenate two arrays using the Spread...Operator

--- JavaScript-and-The-DOM/js/lesson1.js
+++ JavaScript-and-The-DOM/js/lesson1.js
@@ -111,3 +111,15 @@ console.log(...books);
 const primes = new Set([2, 3, 5, 7, 11, 13, 17, 19, 23, 29]);
 console.log(primes);
 console.log(...primes);
+
+
+/*
+ * Instructions: Use the spread operator to combine the `fruits` and `vegetables` arrays into the `produce` array.
+ */
+
+const fruits = ["apples", "bananas", "pears"];
+const vegetables = ["corn", "potatoes", "carrots"];
+
+const produce = [...fruits, ...vegetables];
+
+console.log(produce);
