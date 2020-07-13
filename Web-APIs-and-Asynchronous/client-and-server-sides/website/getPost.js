375804f6cc4c03951a8fb47f474a2934167616be
SondosSamii <sondos.sami.abuzeid@gmail.com>
2020-07-13 13:48:50 +0200

    Add Client Side and Server Side

---
+++ Web-APIs-and-Asynchronous/client-and-server-sides/website/getPost.js
@@ -0,0 +1,17 @@
+// TODO
+const sendData = async (url = "", data = {}) => {
+  const res = await fetch(url, {
+    method: "POST",
+    credentials: "same-origin",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  });
+  try {
+    const newData = await res.json();
+    return newData;
+  } catch (error) {
+    console.log("error", error);
+  }
+};
